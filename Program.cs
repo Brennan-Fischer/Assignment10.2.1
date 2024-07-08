eadd4080f2b5ecedf6d15521a4802ecf99e20968
Brennan Fischer <brennan.fischer@outlook.com>
2024-07-08 22:13:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment10._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, -5, -7, 5, 6, -4, 3, 2, 1, };
+
+            var query =
+                from num in array
+                where num>=0
+                select num;
+            foreach ( var item in query )
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+    }
+}
